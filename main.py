c288d36972a9d3f64e32389f371c21b66919d0e7
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-10-02 12:53:47 +0200

    Pycrypt

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ chars = string.punctuation + string.digits + string.ascii_letters
 chars = list(chars)
 key = chars.copy
 
+random.shuffle(key)
+
 print (f"chars: {chars}")
 print (f"key: {key}")
+
+#ENCRYPT
+plain_text = input("Enter a message to encrypt: ")
