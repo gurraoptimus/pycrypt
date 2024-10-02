e9ce5446fa299458f4819c0dacde25b7b5a74ec1
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-10-02 14:03:56 +0200

    pycrypt

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ key = chars.copy
 
 random.shuffle(key)
 
-print (f"chars: {chars}")
-print (f"key: {key}")
+#print (f"chars: {chars}")
+#print (f"key  : {key}")
 
 #ENCRYPT
 plain_text = input("Enter a message to encrypt: ")
