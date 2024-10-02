6408bb0407573c81c3972390789db190f36c324c
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-10-02 14:19:07 +0200

    pycrypt

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ key = chars.copy()
 
 random.shuffle(key)
 
-print (f"chars: {chars}")
-print (f"key  : {key}")
+# print (f"chars: {chars}")
+# print (f"key  : {key}")
 
 #ENCRYPT
 plain_text = input("Enter a message to encrypt: ")
@@ -20,3 +20,15 @@ for letter in plain_text:
 
 print(f"original message : {plain_text}")
 print(f"encrypted message: {cipher_text}")
+
+
+#ENCRYPT
+cipher_text = input("Enter a message to encrypt: ")
+plain_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"original message : {plain_text}")
