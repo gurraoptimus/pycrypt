4f8cc9601e5220ecf024ebb4ec4b6525c0074163
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-10-02 14:22:28 +0200

    pycrypt

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ print(f"original message : {plain_text}")
 print(f"encrypted message: {cipher_text}")
 
 
-#ENCRYPT
+#DECRYPT
 cipher_text = input("Enter a message to encrypt: ")
 plain_text = ""
 
-for letter in plain_text:
-    index = chars.index(letter)
-    plain_text += chars[index]
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += key[index]
 
 print(f"encrypted message: {cipher_text}")
 print(f"original message : {plain_text}")
